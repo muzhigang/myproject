4ac28763d1c2104248028d62c728837e9e184bf7
Moon <moonmu1230@gmail.com>
2021-09-01 20:14:03 +0800

    Revert "4"
    This reverts commit f69a7ebbaa0f5f6ea036f874a1a958475d097c0d.

--- t1.php
+++ t1.php
@@ -4,14 +4,10 @@
  * @Date:   2021-09-01 15:58:25
  * @Last Modified by:   muzhigang
 <<<<<<< HEAD
-<<<<<<< HEAD
- * @Last Modified time: 2021-09-01 18:21:12
+ * @Last Modified time: 2021-09-01 18:15:44
 =======
  * @Last Modified time: 2021-09-01 18:15:05
 >>>>>>> 3f78ad1 (2)
-=======
- * @Last Modified time: 2021-09-01 18:15:44
->>>>>>> 5d5d3f6 (4)
  */
 echo '1';
 
@@ -19,9 +15,12 @@ echo '44'
 
 1
 
+<<<<<<< HEAD
 2
 
 3
 
 4
-
+=======
+2
+>>>>>>> 3f78ad1 (2)
