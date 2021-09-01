f69a7ebbaa0f5f6ea036f874a1a958475d097c0d
Moon <moonmu1230@gmail.com>
2021-09-01 18:15:51 +0800

    4
    # Conflicts:
    #	t1.php

--- t1.php
+++ t1.php
@@ -4,10 +4,14 @@
  * @Date:   2021-09-01 15:58:25
  * @Last Modified by:   muzhigang
 <<<<<<< HEAD
- * @Last Modified time: 2021-09-01 18:15:44
+<<<<<<< HEAD
+ * @Last Modified time: 2021-09-01 18:21:12
 =======
  * @Last Modified time: 2021-09-01 18:15:05
 >>>>>>> 3f78ad1 (2)
+=======
+ * @Last Modified time: 2021-09-01 18:15:44
+>>>>>>> 5d5d3f6 (4)
  */
 echo '1';
 
@@ -15,12 +19,9 @@ echo '44'
 
 1
 
-<<<<<<< HEAD
 2
 
 3
 
 4
-=======
-2
->>>>>>> 3f78ad1 (2)
+
