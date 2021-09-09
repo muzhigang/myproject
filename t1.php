bc75c55af2329fac465fd95fd1e5fd69da4467ff
Moon <moonmu1230@gmail.com>
2021-09-09 10:40:44 +0800

    1111

--- t1.php
+++ t1.php
@@ -3,11 +3,6 @@
  * @Author: muzhigang
  * @Date:   2021-09-01 15:58:25
  * @Last Modified by:   muzhigang
-<<<<<<< HEAD
- * @Last Modified time: 2021-09-01 18:15:44
-=======
- * @Last Modified time: 2021-09-01 18:15:05
->>>>>>> 3f78ad1 (2)
  */
 echo '1';
 
@@ -15,12 +10,8 @@ echo '44'
 
 1
 
-<<<<<<< HEAD
 2
 
 3
 
 4
-=======
-2
->>>>>>> 3f78ad1 (2)
